b14e790aca7f06e168c97a1b8641906656ebebde
Renato Neto <skapemr@gmail.com>
2015-09-16 15:40:54 -0300

    teste
    teste

--- src/android/TesseractPlugin.java
+++ src/android/TesseractPlugin.java
@@ -29,26 +29,104 @@ public class TesseractPlugin extends CordovaPlugin {
 	public static final String ACTION_ADD_TESSERACT_ENTRY = "addTesseractPluginEntry";
 	public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString();
 	private static final String lang = "eng";
+  private static final String TAG = "SimpleAndroidOCR.java";
 			//.getExternalStorageDirectory().toString() + "/PhoneGapTessTwo/";
 
 	@Override
 	public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
-		try {
+
 			if (ACTION_ADD_TESSERACT_ENTRY.equals(action)) {
+
+        if (!(new File(DATA_PATH + "/tessdata/eng.traineddata")).exists()) {
+            callbackContext.error(DATA_PATH + "/tessdata/eng.traineddata não encontrado");
+            return false;
+        }
+
 				//JSONObject arg_object = args.getJSONObject(0);
-				
+
 				String imagePath = args.getString(0);
-				this.echo(imagePath, callbackContext);
+				//String imagePath = DATA_PATH + "/cracha.jpg";
+
+        if (imagePath.equals("storage")) {
+            imagePath = DATA_PATH + "/cracha.jpg";
+        }
+
+        Log.v(TAG, "imagePath: " + imagePath);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+
+        try {
+          ExifInterface exif = new ExifInterface(imagePath);
+          int exifOrientation = exif.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL);
+
+          Log.v(TAG, "Orient: " + exifOrientation);
+
+          int rotate = 0;
+
+          switch (exifOrientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+              rotate = 90;
+              break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+              rotate = 180;
+              break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+              rotate = 270;
+              break;
+          }
+
+          Log.v(TAG, "Rotation: " + rotate);
+
+          if (rotate != 0) {
+
+            // Getting width & height of the given image.
+            int w = bitmap.getWidth();
+            int h = bitmap.getHeight();
+
+            // Setting pre rotate
+            Matrix mtx = new Matrix();
+            mtx.preRotate(rotate);
+
+            // Rotating Bitmap
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+          }
+
+          // Convert to ARGB_8888, required by tess
+          bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+          Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        Log.v(TAG, "Before baseApi");
+
+				TessBaseAPI baseApi = new TessBaseAPI();
+				baseApi.setDebug(true);
+				baseApi.init(DATA_PATH, lang);
+				baseApi.setImage(bitmap);
+
+				String recognizedText = baseApi.getUTF8Text();
+				baseApi.end();
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if (recognizedText == null) {
+          callbackContext.error("Cracha não encontrado");
+          return false;
+        }
+
+				this.echo(recognizedText, callbackContext);
 				return true;
 			}
+
 			callbackContext.error("Invalid action");
 			return false;
-		} catch (Exception e) {
-			System.err.println("Exception in Execute: " + e.getMessage());
-			callbackContext.error(e.getMessage());
-			return false;
-		}
 	}
 	private void echo(String imagePath, CallbackContext callbackContext) {
         if (imagePath != null && imagePath.length() > 0) {
@@ -57,35 +135,34 @@ public class TesseractPlugin extends CordovaPlugin {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
-	
 
-	public String TesseractExampleTwo()
+
+	public String TesseractExampleTwo(String pathToPic)
 	{
 		String recognizedText = "";
-		String pathToPic = DATA_PATH + "/PhoneGapTessTwo" + "/tessdata" + "/eurotext4.png";
 		String pathToPath = DATA_PATH + "/PhoneGapTessTwo/";
-		
+
 		try {
-			
+
 		System.err.println("Before BitMapFactory.Options");
-			
+
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 4;
 
 		System.err.println("Before bitmap = decodeFile");
 		Bitmap bitmap = BitmapFactory.decodeFile(pathToPic, options);
-		
-		
-		
+
+
+
 		System.err.println("Before bitmap.copy");
-		
+
 		try {
 			ExifInterface exif = new ExifInterface(pathToPic);
 			int exifOrientation = exif.getAttributeInt(
 					ExifInterface.TAG_ORIENTATION,
 					ExifInterface.ORIENTATION_NORMAL);
 
-			//Log.v(TAG, "Orient: " + exifOrientation);
+			Log.v(TAG, "Orient: " + exifOrientation);
 			System.err.println("Orient: " + exifOrientation);
 
 			int rotate = 0;
@@ -102,7 +179,7 @@ public class TesseractPlugin extends CordovaPlugin {
 				break;
 			}
 
-			//Log.v(TAG, "Rotation: " + rotate);
+			Log.v(TAG, "Rotation: " + rotate);
 			System.err.println("Rotation: " + rotate);
 
 			if (rotate != 0) {
@@ -123,20 +200,20 @@ public class TesseractPlugin extends CordovaPlugin {
 			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
 
 		} catch (IOException e) {
-			//Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+			Log.e(TAG, "Couldn't correct orientation: " + e.toString());
 			System.err.println("Couldnt correct orientation");
 		}
-						
-		//Log.v(TAG, "Before baseApi");
-		
-		
+
+		Log.v(TAG, "Before baseApi");
+
+
 		System.err.println("Before baseApi");
 
 		TessBaseAPI baseApi = new TessBaseAPI();
 		baseApi.setDebug(true);
-		
+
 		System.err.println("Before baseApi.init");
-		baseApi.init(pathToPath, lang);
+		baseApi.init(DATA_PATH, lang);
 		//baseApi.setImage(bitmap);
 		//File imageFile = new File(bitmap);
 		System.err.println("Before baseApi.setImage");
@@ -152,7 +229,7 @@ public class TesseractPlugin extends CordovaPlugin {
 
 		System.err.println("Before baseApi.end");
 		baseApi.end();
-		
+
 		}
 		catch (Exception e){
 			System.err.println("TesseractTwoExample: " + e);
@@ -162,13 +239,13 @@ public class TesseractPlugin extends CordovaPlugin {
 		// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
 		// so that garbage doesn't make it to the display.
 
-		//Log.v(TAG, "OCRED TEXT: " + recognizedText);
+		Log.v(TAG, "OCRED TEXT: " + recognizedText);
 		System.err.println("OCRED TEXT: " + recognizedText);
 
 		//if ( lang.equalsIgnoreCase("eng") ) {
 		//	recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
 		//}
-		
+
 		return recognizedText;
 		//recognizedText = recognizedText.trim();
 
@@ -177,22 +254,22 @@ public class TesseractPlugin extends CordovaPlugin {
 	//		_field.setSelection(_field.getText().toString().length());
 	//	}
 
-		
+
 	}
-	
-	
+
+
 	public String TesseractExample() {
-		
+
 		String recognizedText = "";
 		//_path = DATA_PATH + "/ocr.jpg";
-		
+
 		try
 		{
 		File imageFile = new File("eurotext.png");
 		if(imageFile.exists() != true)
 		{
 			System.err.println("Image: FALSE");
-			
+
 		}
 		else{
 			System.err.println("Image: TRUE");
@@ -203,19 +280,19 @@ public class TesseractPlugin extends CordovaPlugin {
 		// getExternalFilesDir(Environment.MEDIA_MOUNTED);
 
 		// lang = for which the language data exists, usually "eng"
-		TessBaseAPI baseApi = new TessBaseAPI();	
-		
+		TessBaseAPI baseApi = new TessBaseAPI();
+
 
 		// DATA_PATH = Path to the storage
 		baseApi.init(DATA_PATH, "eng.traineddata"); // myDir + "/tessdata/eng.traineddata"
 								// must be present
 		//baseApi.setDebug(true);
 		System.err.println("Pass Init: "+DATA_PATH+"/eng.traineddata");
- 
-		
+
+
 		// Eg. baseApi.init("/mnt/sdcard/tesseract/tessdata/eng.traineddata",
 		// "eng");
-		
+
 		System.err.println("Pass SetImage: ");
 		baseApi.setImage(imageFile);
 		System.err.println("Pass SetImage: ");
@@ -225,16 +302,16 @@ public class TesseractPlugin extends CordovaPlugin {
 														// display this
 														// string...
 		baseApi.end();
-		
+
 		}
 		catch (Exception e){
 			System.err.println("Exception: " + e.getMessage());
-			
+
 			recognizedText = "FEJL";
-		
+
 		}
 		return recognizedText;
-		
+
 	}
 
 	private void SampleTest(String sampletest, CallbackContext callbackContext) {
@@ -245,7 +322,7 @@ public class TesseractPlugin extends CordovaPlugin {
 		}
 
 	}
-	
-	
+
+
 
 }
