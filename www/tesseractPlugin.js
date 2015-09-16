dcb32ce7784101545f9d88ff6a02fed1991884ed
Renato Neto <skapemr@gmail.com>
2015-09-16 15:57:16 -0300

    Update tesseractPlugin.js

--- www/tesseractPlugin.js
+++ www/tesseractPlugin.js
@@ -1,13 +1,13 @@
-var tesseractPlugin = {
+var tesseract = {
     createEvent: function(str, Callback) {
         cordova.exec(callback, function(err) {
 		callback('Nothing to echo.');
 			},
 			"TesseractPlugin",			//Service: Mapped to native java.class
 			"addTesseractPluginEntry",	//Action: The action name to call into on the native side
-			[{							//An array of arguments 
+			[							//An array of arguments 
 			str							//This string should contain path to image
-			}]);						          
+			]);						          
      }
 }
 module.exports = tesseract;
